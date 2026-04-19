dd344f3bfb5dbeda136f6170b086b7e61b493d26
Hemanshu Mahajan <hemanshumahajan21@gmail.com>
2026-04-19 12:30:06 +0530

    ignore

--- TestGenApi/Program.cs
+++ TestGenApi/Program.cs
@@ -23,6 +23,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 app.UseCors("AllowFrontend");
